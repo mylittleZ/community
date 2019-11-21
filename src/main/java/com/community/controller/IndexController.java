f750715f398b17cf21ddb60eeeaaa9a897e5f06f
mylittleZ <1020464190@qq.com>
2019-11-21 15:28:46 +0800

    add navbar

--- src/main/java/com/community/controller/IndexController.java
+++ src/main/java/com/community/controller/IndexController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 //controller就是说允许这个类去接收前端的请求
 @Controller
 
-public class HelloController {
-    @GetMapping("/hello")
-   public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);//把浏览器中传过来的值放到model里面
-       return "hello";
+public class IndexController {
+    @GetMapping("/index")
+   public String index(){
+        //model.addAttribute("name",name);//把浏览器中传过来的值放到model里面
+       return "index";//返回index.html这个模板
    }
 }
